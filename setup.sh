ac3d82362dea8a5c7e1a3ddb56aab332db44f317
Renan Rezenes <rezeness@gmail.com>
2018-04-22 14:50:51 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,19 +4,19 @@ echo Uploading Application container
 docker-compose up -d
 
 echo Copying the configuration example file
-docker exec -it docker_php_1 cp .env.example .env
+docker exec -it docker-server-cliente_php_1 cp .env.example .env
 
 echo Install dependencies
-docker exec -it docker_php_1 composer install
+docker exec -it docker-server-cliente_php_1 composer install
 
 echo Generate key
-docker exec -it docker_php_1 php artisan key:generate
+docker exec -it docker-server-cliente_php_1 php artisan key:generate
 
 echo Make migrations
-docker exec -it docker_php_1 php artisan migrate
+docker exec -it docker-server-cliente_php_1 php artisan migrate
 
 echo Make seeds
-docker exec -it docker_php_1 php artisan db:seed
+docker exec -it docker-server-cliente_php_1 php artisan db:seed
 
 echo Information of new containers
 docker ps -a

ec242423bee5fe0b77fcf25a837169aa586e4a03
Renan Rezenes <rezeness@gmail.com>
2018-04-22 19:14:34 -0700

    atualização nome do container

--- setup.sh
+++ setup.sh
@@ -3,19 +3,19 @@
 echo Uploading Application container 
 docker-compose up -d
 
-docker exec -it ${PWD##*/}_php_1 cp .env.example .env
+docker exec -it php_7 cp .env.example .env
 
 echo Install dependencies
-docker exec -it ${PWD##*/}_php_1 composer install
+docker exec -it php_7 composer install
 
 echo Generate key
-docker exec -it ${PWD##*/}_php_1 php artisan key:generate
+docker exec -it php_7 php artisan key:generate
 
 echo Make migrations
-docker exec -it ${PWD##*/}_php_1 php artisan migrate
+docker exec -it php_7 php artisan migrate
 
 echo Make seeds
-docker exec -it ${PWD##*/}_php_1 php artisan db:seed
+docker exec -it php_7 php artisan db:seed
 
 echo Information of new containers
 docker ps -a
